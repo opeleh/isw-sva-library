61689060c31520c4b2cf6bfaee64f5814c599d91
Fola Akinbobola <voachamber@gmil.com>
2021-08-26 19:25:28 +0100

    project creation

---
+++ src/main/java/io/opeleh/iswsvalibrary/IswSvaLibraryApplication.java
@@ -0,0 +1,13 @@
+package io.opeleh.iswsvalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IswSvaLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IswSvaLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/opeleh/iswsvalibrary/IswSvaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.opeleh.iswsvalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IswSvaLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
